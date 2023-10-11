1131d3c4f63fb022599f8e332161054cf4fe7cec
unknown <albertkantwi@gmail.com>
2023-10-11 16:55:50 +0100

    Update person, student and teacher files

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_accessor :specialization
-
-  def initialize(id, age, name = 'Unknown', parent_permission: true, specialization: 'Unknown')
-    super(id, age, name, parent_permission: parent_permission)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
